0bd42f8212e42e45fc404f607996f57011252dc7
Markus Eberl <markus.eberl@hotmail.com>
2024-10-18 09:18:21 +0200

    WIP: Add gguf support.

--- entropix/torch_weights.py
+++ entropix/torch_weights.py
@@ -10,6 +10,8 @@ import ml_dtypes
 
 from pathlib import Path
 
+from gguf import GGUFReader, ReaderTensor
+
 # Device selection, tree is like first apple silicion, then cuda, fallback is cpu.
 if torch.backends.mps.is_available():
     device = torch.device("mps")
@@ -81,3 +83,48 @@ def load_weights(ckpt_dir: Path = Path('weights/1B-Instruct'), n_layers: int = 1
     )
 
     return xfmr_weights
+  
+def load_gguf_weights(path: str = "Llama-3.2-1B-Instruct-Q4_K_M.gguf"):
+  reader = GGUFReader(path)
+  block_count = reader.fields.items()["llama.block_count"]
+  layer_weights = []
+
+  reader.tensors
+
+    #   struct ggml_tensor * ffn_gate; // w1
+    # struct ggml_tensor * ffn_down; // w2
+    # struct ggml_tensor * ffn_up;   // w3
+
+  for i in range(block_count):
+    tensors_at_layer_i = [t for t in reader.tensors if f"{i}" in t.name]
+
+    layer_weights.append(LayerWeights(
+      wq=get_right_tensor(name=f'blk.{i}.attn_q.weight', tensors=tensors_at_layer_i),
+      wk=get_right_tensor(name=f'blk.{i}.attn_k.weight', tensors=tensors_at_layer_i),
+      wv=get_right_tensor(name=f'blk.{i}.attn_v.weight', tensors=tensors_at_layer_i),
+      wo=get_right_tensor(name=f'blk.{i}.attn_output.weight', tensors=tensors_at_layer_i),
+      w1=get_right_tensor(name=f'blk.{i}.ffn_gate.weight', tensors=tensors_at_layer_i),
+      w2=get_right_tensor(name=f'blk.{i}.ffn_down.weight', tensors=tensors_at_layer_i),
+      w3=get_right_tensor(name=f'blk.{i}.ffn_up.weight', tensors=tensors_at_layer_i),
+      ffn_norm=get_right_tensor(name=f'blk.{i}.ffn_norm.weight', tensors=tensors_at_layer_i),
+      attention_norm=get_right_tensor(name=f'blk.{i}.attn_norm.weight', tensors=tensors_at_layer_i)
+
+      # wq=w[f'layers.{i}.attention.wq.weight'],
+      # wk=w[f'layers.{i}.attention.wk.weight'],
+      # wv=w[f'layers.{i}.attention.wv.weight'],
+      # wo=w[f'layers.{i}.attention.wo.weight'],
+      # w1=w[f'layers.{i}.feed_forward.w1.weight'],
+      # w2=w[f'layers.{i}.feed_forward.w2.weight'],
+      # w3=w[f'layers.{i}.feed_forward.w3.weight'],
+      # ffn_norm=w[f'layers.{i}.ffn_norm.weight'],
+      # attention_norm=w[f'layers.{i}.attention_norm.weight']
+    ))
+  xfmr_weights = XfmrWeights(
+    tok_embeddings=get_right_tensor(name='token_embd.weight', tensors=reader.tensors),
+    norm=w['norm.weight'],
+    output=get_right_tensor(name='output_norm.weight', tensors=reader.tensors),
+    layer_weights=layer_weights
+  )
+
+def get_right_tensor(name: str, tensors: List[ReaderTensor]):
+  return None
